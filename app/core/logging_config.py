598bac94bc7f29e4fd47e294ef9676be8aad7761
KIKIMORA <sasha.kovalov2008@gmail.com>
2025-08-05 18:39:20 +0300

    add logs save

--- app/core/logging_config.py
+++ app/core/logging_config.py
@@ -10,12 +10,18 @@ logging.basicConfig(
     level=logging.INFO
 )
 
+file_handler = logging.FileHandler("app.log")
+file_handler.setFormatter(logging.Formatter("%(message)s"))
+
+base_logger = logging.getLogger("app")
+base_logger.addHandler(file_handler)
+
 logger = wrap_logger(
-    logging.getLogger("app"),
+    base_logger,
     processors=[
+        add_contextvars,
         processors.TimeStamper(fmt="iso"),
         processors.add_log_level,
-        add_contextvars,
         processors.StackInfoRenderer(),
         processors.format_exc_info,
         processors.CallsiteParameterAdder([
